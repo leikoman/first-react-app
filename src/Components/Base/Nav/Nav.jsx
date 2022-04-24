de4ce0b2f61b38335a8e2bb1ac6fd1d9af51245f
leikoman <leikoman@gmail.com>
2022-04-24 22:14:18 +0300

    add style for active link, gitignore

--- src/Components/Base/Nav/Nav.jsx
+++ src/Components/Base/Nav/Nav.jsx
@@ -1,36 +1,44 @@
 import React from "react";
 import css from './Nav.module.css';
+import { NavLink } from 'react-router-dom';
+
 
 const Nav = () => {
     return (
         <nav className={css.nav}>
             <div className={css.item}>
-                <a href="/news">
+                <NavLink to="/news" className={SelectedLink()}>
                     <img alt="News" src="./././nav-news.jpg"></img>
-                </a>
+                </NavLink>
             </div>
             <div className={css.item}>
-                <a href="/dialogs">
+                <NavLink to="/dialogs" className={SelectedLink()}>
                     <img alt="Messages" src="./././nav-messages.jpg"></img>
-                </a>
+                </NavLink>
             </div>
             <div className={css.item}>
-                <a href="/photo">
+                <NavLink to="/photo" className={SelectedLink()}>
                     <img alt="Photo" src="./././nav-photo.jpg"></img>
-                </a>
+                </NavLink>
             </div>
             <div className={css.item}>
-                <a href="/music">
+                <NavLink to="/music" className={SelectedLink()}>
                     <img alt="Music" src="./././nav-music.jpg"></img>
-                </a>
+                </NavLink>
             </div>
             <div className={css.item}>
-                <a href="/">
+                <NavLink to="/" className={SelectedLink()}>
                     <img alt="About Me" src="./././nav-settings.jpg"></img>
-                </a>
+                </NavLink>
             </div>
         </nav>
     )
 }
-
 export default Nav
+
+
+const SelectedLink = () => {
+    return (
+        select => select.isActive ? css.activeLink : css.item
+    )
+}
