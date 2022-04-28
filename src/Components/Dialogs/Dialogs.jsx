e42576cd7b2f34916148a1b219ddf711a91a463a
leikoman <leikoman@gmail.com>
2022-04-28 23:28:41 +0300

    dialogs layout

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import css from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div className={css}>
-            dialogs brrr
+        <div className={css.DialogsPage}>
+            <div className={css.Dialogs}>
+                <div className={css.ActiveDialog}>Vasya</div>
+                <div className={css.Dialog}>Petya</div>
+                <div className={css.Dialog}>Semen</div>
+                <div className={css.Dialog}>Igor</div>
+                <div className={css.Dialog}>Petr</div>
+                <div className={css.Dialog}>Tanya</div>
+                <div className={css.Dialog}>Katya</div>
+                <div className={css.Dialog}>Sveta</div>
+                <div className={css.Dialog}>Nastya</div>
+                <div className={css.Dialog}>Innokentii</div>
+            </div>
+            <div className={css.messages}>
+                <div className={css.message}>Hi!</div>
+                <div className={css.message}>What are u doing?</div>
+                <div className={css.message}>How are u?</div>
+                <div className={css.message}>Let's go!</div>
+                <div className={css.message}>Bye!</div>
+            </div>
         </div>
     );
 }
